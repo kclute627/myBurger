30f57bda01839fd904e2742cff0530c95282ac16
Kyle C <kyclutter@gmail.com>
2018-12-05 17:12:42 -0600

    new additions to Burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,16 +1,24 @@
 import React, {Component} from 'react';
 
 import Auxx from '../../hoc/Auxx';
-
+import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
+
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        },
+    }
 
     render(){
 
         return(
             <Auxx>
-                <div>Burger</div>
-                <div>Build Controls</div>
+                <Burger ingredients = {this.state.ingredients}  />
             </Auxx>
         );
     }
