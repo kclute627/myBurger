b268f5ba1cb7ed4a7bd2305191b76c9f099747c3
Kyle C <kyclutter@gmail.com>
2019-01-27 09:44:17 -0600

    updating authorization

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,9 @@ import BurgerBuilder from '../containers/BurgerBuilder/BurgerBuilder';
 import Layout from '../components/Layout/Layout';
 import Checkout from '../containers/Checkout/Checkout';
 import Orders from '../containers/Orders/Orders';
+import Auth from '../containers/Auth/Auth';
+
+
 
 class App extends Component {
  
@@ -17,6 +20,7 @@ class App extends Component {
             
             <Route path='/checkout' component ={Checkout}/>
             <Route path='/orders' component ={Orders}/>
+            <Route path='/auth' component ={Auth}/>
             <Route path='/' exact component ={BurgerBuilder}/>
             
 
---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,133 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Input from '../../components/UI/Input/input';
+import Button from '../../components/UI/Button/Button';
+import * as actions from '../../store/actions/index';
+import classes from './auth.css';
+
+
+
+
+class Auth extends Component {
+
+    state ={
+        controls: {            
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Email'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    isEmail: true
+                },
+                valid: false,
+                touched: false
+            },
+            password: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'password',
+                    placeholder: 'Password'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 6
+                },
+                valid: false,
+                touched: false
+            },
+        }
+
+    }
+    
+    checkValidity(value, rules){
+            
+        let isValid = true;
+                    
+        if(rules.required){
+            isValid = value.trim() !== '' && isValid; 
+        }
+        if(rules.minLength){
+            isValid = value.length >= rules.minLength && isValid
+        }
+        if(rules.maxLength){
+            isValid = value.length <= rules.maxLength && isValid
+        }
+
+        return isValid
+
+    }
+
+    inputChangedHandler = (event, controlName)=>{
+        const updatedControls = {
+            ...this.state.controls,
+            [controlName]: {
+                ...this.state.controls[controlName],
+                value: event.target.value,
+                valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+                touched: true,
+            }
+        };
+
+        this.setState({controls: updatedControls})
+
+
+
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault();
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value)
+    }
+
+    render(){
+
+        const formElementsArray = [];
+        for(let key in this.state.controls){
+            formElementsArray.push({
+                id: key,
+                config: this.state.controls[key]
+            });
+
+
+        }
+
+        const form = formElementsArray.map(cur => (
+            <Input
+            key={cur.id}
+            elementType={cur.config.elementType}
+            elementConfig = {cur.config.elementConfig}
+            value= {cur.config.value}
+            invalid = {!cur.config.valid}
+            shouldValidate = {cur.config.validation}
+            touched={cur.config.touched} 
+            changed ={(event) => this.inputChangedHandler(event, cur.id)}/>
+        
+        ))
+
+        return(
+            <div className={classes.Auth}>
+                <form 
+                onSubmit = {this.submitHandler}>
+                    {form}
+                    <Button btnType='Success'>Submit</Button>
+                </form>
+            </div>
+        );
+    }
+}
+
+
+const mapDispatchToProps = (dispatch) =>{
+    return{
+        onAuth: (email, password)=>dispatch(actions.auth(email, password))
+
+    }
+}
+
+
+export default connect(null, mapDispatchToProps)(Auth);
